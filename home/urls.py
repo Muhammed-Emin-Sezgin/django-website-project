8da419963aad05d53f135c60e826021a7385f09a
EminSezgin <muhammed.emin.293@gmail.com>
2020-03-30 01:34:43 +0300

    first commit

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,22 @@ from . import views
 urlpatterns = [
     # ex: /home/
     path('', views.index, name='index'),
+    path('index.html', views.index, name='index'),
+    path('about.html', views.about, name='about'),
+    path('blog.html', views.blog, name='blog'),
+    path('blog-single.html', views.blog_single, name='blog-single'),
+    path('contact.html', views.contact, name='contact'),
+    path('faq.html', views.faq, name='faq'),
+    path('gallery.html', views.gallery, name='gallery'),
+    path('job-listings.html', views.job_listings, name='job-listings'),
+    path('job-single.html', views.job_single, name='job-single'),
+    path('login.html', views.login, name='login'),
+    path('main.html', views.main, name='main'),
+    path('portfolio.html', views.portfolio, name='portfolio'),
+    path('portfolio-single.html', views.portfolio_single, name='portfolio-single'),
+    path('post-job.html', views.post_job, name='post-job'),
+    path('service-single.html', views.service_single, name='service-single'),
+    path('services.html', views.services, name='services'),
+    path('testimonials.html', views.testimonials, name='testimonials'),
+
 ]
--- home/views.py
+++ home/views.py
@@ -3,10 +3,75 @@ from django.http import HttpResponse
 
 
 def index(request):
-    text = "Hello Django"
-    bolum = "Computer Engineering"
+    #text = "Hello Django"
+    #bolum = "Computer Engineering"
 
-    context = {'text': text, 'bolum': bolum}
-    return render(request, 'index.html', context)
+    #context = {'text': text, 'bolum': bolum}
+    return render(request, 'index.html')
 
 # Create your views here.
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def blog(request):
+    return render(request, 'blog.html')
+
+
+def blog_single(request):
+    return render(request, 'blog-single.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def faq(request):
+    return render(request, 'faq.html')
+
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+
+def job_listings(request):
+    return render(request, 'job-listings.html')
+
+
+def job_single(request):
+    return render(request, 'job-single.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def main(request):
+    return render(request, 'main.html')
+
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
+
+
+def portfolio_single(request):
+    return render(request, 'portfolio-single.html')
+
+
+def post_job(request):
+    return render(request, 'post-job.html')
+
+
+def service_single(request):
+    return render(request, 'service-single.html')
+
+
+def services(request):
+    return render(request, 'services.html')
+
+
+def testimonials(request):
+    return render(request, 'testimonials.html')
+
