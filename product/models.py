125c6b2a4935030524c2b1463248e5105b5dc5dd
EminSezgin <muhammed.emin.293@gmail.com>
2020-03-30 05:01:19 +0300

    first commit

--- product/models.py
+++ product/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 # Create your models here.
+from django.utils.safestring import mark_safe
+
+
 class Category(models.Model):
 
     STATUS = (
@@ -21,6 +24,10 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
 
 
 class Product(models.Model):
@@ -45,6 +52,10 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
+
 
 class Images(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -53,3 +64,7 @@ class Images(models.Model):
 
     def __str__(self):
         return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
